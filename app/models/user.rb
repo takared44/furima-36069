8ab72f3358a21203d891d0b2ee88c4527f0ef4c4
takared44 <86856476+takared44@users.noreply.github.com>
2021-08-02 20:34:01 +0900

    全角文字、全角カナバリデード設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,13 @@ class User < ApplicationRecord
  
 
 validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください'}
- with_options presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください'} do
-   
-   
+ with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角文字を使用してください'} do
+   validates :first_name
+   validates :last_name
   end
-  with_options presence: true, format:{ with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください'}do
-    
-    
+  with_options presence: true, format:{ with: /\A[ァ-ヶー－]+\z/, message: 'はカタカナを使用して入力してください'}do
+    validates :last_name_kana
+    validates :first_name_kana
   end
 
  has_many :items
