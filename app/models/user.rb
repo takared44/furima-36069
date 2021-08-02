d50b3cb7a36b7a047cd61294984fbecef32e29b9
takared44 <86856476+takared44@users.noreply.github.com>
2021-08-02 20:14:56 +0900

    名前とカナのバリデードを消した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,12 @@ class User < ApplicationRecord
 
 validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください'}
  with_options presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください'} do
-   validates :first_name
-   validates :last_name
+   
+   
   end
   with_options presence: true, format:{ with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください'}do
-    validates :last_name_kana
-    validates :first_name_kana
+    
+    
   end
 
  has_many :items
