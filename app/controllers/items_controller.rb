3db37c34a70759005e8867ef3cd722b9a75781cb
takared44 <86856476+takared44@users.noreply.github.com>
2021-08-02 20:35:17 +0900

    ルボコップ完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,4 @@
 class ItemsController < ApplicationController
-
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
 
   before_action :find_item, only: [:show, :edit, :update, :destroy]
@@ -9,33 +8,30 @@ class ItemsController < ApplicationController
   before_action :sold_out, only: [:edit]
 
   def index
-    #@items = Item.all.order(id: 'DESK')
+    # @items = Item.all.order(id: 'DESK')
   end
 
   def new
     @items = Item.new
-end
+  end
 
-def create
-   @item = Item.new(item_params)
-   if @item.valid?
-    @item.save
-    redirect_to root_path
-   else
-    render :new
-   end
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
-
   end
 
   def edit
-
   end
 
   def update
-
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -48,21 +44,22 @@ def create
     redirect_to root_path
   end
 
-    private
+  private
 
-    def item_params
-      params.require(:item).permit(:product_name, :text, :category_id, :state_id, :delivery_fee_id, :delivery_prefecture_id, :delivery_date_id, images: []).merge(user_id: current_user.id)
-    end
-
-    def find_item
-      @item = Item.find(params[:id])
-    end
-
-    def move_to_root_path
-      redirect_to root_path if current_user.id != @item.user.id
-    end
-
-    def sold_out
-      redirect_to root_path if @item.record != nil
-    end
+  def item_params
+    params.require(:item).permit(:product_name, :text, :category_id, :state_id, :delivery_fee_id, :delivery_prefecture_id,
+                                 :delivery_date_id, images: []).merge(user_id: current_user.id)
   end
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_root_path
+    redirect_to root_path if current_user.id != @item.user.id
+  end
+
+  def sold_out
+    redirect_to root_path unless @item.record.nil?
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,19 +7,18 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :birthday
- end
- 
-
-validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください'}
- with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角文字を使用してください'} do
-   validates :first_name
-   validates :last_name
   end
-  with_options presence: true, format:{ with: /\A[ァ-ヶー－]+\z/, message: 'はカタカナを使用して入力してください'}do
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください' }
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'はカタカナを使用して入力してください' } do
     validates :last_name_kana
     validates :first_name_kana
   end
 
- has_many :items
- has_many :records
-end 
+  has_many :items
+  has_many :records
+end
