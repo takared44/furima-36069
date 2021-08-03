4c036292e9a578d9322c24c9f6255e1b4ebdfdc0
takared44 <86856476+takared44@users.noreply.github.com>
2021-08-03 12:53:49 +0900

    テストコード修正しました。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,119 +18,119 @@ describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("ニックネームを入力してください")
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
     end
     it 'emailが空では登録できない' do
       @user.email = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Eメールを入力してください")
+      expect(@user.errors.full_messages).to include("Email can't be blank")
     end
     it 'passwordが空では登録できない' do
       @user.password = ''
       @user.valid?
-      expect(@user.errors.full_messsages).to include("パスワードを入力してください")
+      expect(@user.errors.full_messages).to include("Password can't be blank")
     end
     it 'passwordが存在してもpassword_confirmationが空では登録できない' do
       @user.password_confirmation = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
     it 'last_nameが空では登録できない' do
       @user.last_name = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("名字を入力してください")
+      expect(@user.errors.full_messages).to include("Last name は全角文字を使用してください")
     end
     it 'first_nameが空では登録できない' do
       @user.first_name = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("名前を入力してください")
+      expect(@user.errors.full_messages).to include("First name は全角文字を使用してください")
     end
     it 'last_name_kanaが空では登録できない' do
       @user.last_name_kana = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("名字(カナ)はカタカナを使用して入力してください")
+      expect(@user.errors.full_messages).to include("Last name kana はカタカナを使用して入力してください")
     end
     it 'first_name_kanaが空では登録できない' do
       @user.first_name_kana = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("名前(カナ)はカタカナを使用して入力してください")
+      expect(@user.errors.full_messages).to include("First name kana はカタカナを使用して入力してください")
     end
     it 'birthdayが空では登録できない' do
       @user.birthday = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("誕生日を入力してください")
+      expect(@user.errors.full_messages).to include("Birthday can't be blank")
     end
 
     it 'emailは@を含んでいないと登録できない' do
       @user.email = 'aaa.gmail.com'
       @user.valid?
-      expect(@user.errors.full_messages).to include('Eメールは不正な値です')
+      expect(@user.errors.full_messages).to include('Email is invalid')
     end
     it '重複したemailが存在する場合登録できない' do
       @user.save
       another_user = FactoryBot.build(:user)
       another_user.email = @user.email
       another_user.valid?
-      expect(another_user.errors.full_messages).to include('Eメールはすでに存在します')
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
     end
     it 'passwordが半角英数混合でなければ登録できない' do
       @user.password = '111111'
       @user.valid?
-      expect(@user.errors.full_messages).to include('パスワードは半角英数混合を使用してください')
+      expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
     end
     it 'passwordが半角英数混合でなければ登録できない' do
       @user.password = 'aaaaaa'
       @user.valid?
-      expect(@user.errors.full_messages).to include('パスワードは半角英数混合を使用してください')
+      expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
     end
     it 'passwordが全角では登録できない' do
       @user.password = 'AAAAAA'
       @user.valid?
-      expect(@user.errors.full_messages).to include('パスワードは半角英数混合を使用してください')
+      expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
     end
     it 'passwordが5文字以下では登録できない' do
       @user.password = '111aa'
       @user.valid?
-      expect(@user.errors.full_messages).to include('パスワードは6文字以上で入力してください')
+      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
     it 'passwordとpassword_confirmationが一致してないと登録できない' do
       @user.password_confirmation = '111111a'
       @user.password == @user.password_confirmation
       @user.valid?
-      expect(@user.errors.full_messages).to include("パスワード(確認用)とパスワードの入力が一致しません")
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
 
     it 'ユーザー本名(名字)は英語ではできない' do
       @user.last_name = 'Yamada'
       @user.valid?
-      expect(@user.errors.full_messages).to include('名字は全角文字を使用してください')
+      expect(@user.errors.full_messages).to include('Last name は全角文字を使用してください')
     end
     it 'ユーザー本名(名前)は英語ではできない' do
       @user.first_name = 'tarou'
       @user.valid?
-      expect(@user.errors.full_messages).to include('名前は全角文字を使用してください')
+      expect(@user.errors.full_messages).to include('First name は全角文字を使用してください')
     end
 
     it 'ユーザー本名(カナ名字)は漢字ではできない' do
       @user.last_name_kana = '山田'
       @user.valid?
-      expect(@user.errors.full_messages).to include('名字(カナ)はカタカナを使用してください')
+      expect(@user.errors.full_messages).to include('Last name kana はカタカナを使用して入力してください')
     end
     it 'ユーザー本名(カナ名字)は英語ではできない' do
       @user.last_name_kana = 'yamada'
       @user.valid?
-      expect(@user.errors.full_messages).to include('名字(カナ)はカタカナを使用してください')
+      expect(@user.errors.full_messages).to include('Last name kana はカタカナを使用して入力してください')
     end
 
     it 'ユーザー本名(カナ名前)は漢字ではできない' do
       @user.first_name_kana = '太郎'
       @user.valid?
-      expect(@user.errors.full_messages).to include('名前(カナ)はカタカナを使用してください')
+      expect(@user.errors.full_messages).to include('First name kana はカタカナを使用して入力してください')
     end
     it 'ユーザー本名(カナ名前)は英語ではできない' do
       @user.first_name_kana = 'tarou'
       @user.valid?
-      expect(@user.errors.full_messages).to include('名前(カナ)はカタカナを使用してください')
+      expect(@user.errors.full_messages).to include('First name kana はカタカナを使用して入力してください')
     end
   end
 end
