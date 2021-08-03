156ad7106a479082a0526ba346f1e569a3465875
takared44 <86856476+takared44@users.noreply.github.com>
2021-08-03 15:17:27 +0900

    見やすく修正しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,18 +7,17 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :birthday
+    
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください' }
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字を使用してください' } do
+      validates :first_name
+      validates :last_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'はカタカナを使用して入力してください' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
   end
-
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください' }
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ケ一-龥々ー]+\z/, message: 'は全角文字を使用してください' } do
-    validates :first_name
-    validates :last_name
-  end
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'はカタカナを使用して入力してください' } do
-    validates :last_name_kana
-    validates :first_name_kana
-  end
-
   #has_many :items
   #has_many :records
 end
