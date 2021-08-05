42d27bc6ea93f2e45724f87bb67822d830748c94
takared44 <86856476+takared44@users.noreply.github.com>
2021-08-05 17:14:55 +0900

    onlyオプション修正しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, ":edit, :update, :destroy"]
 
   before_action :find_item, only: [:show, :edit, :update, :destroy]
 
