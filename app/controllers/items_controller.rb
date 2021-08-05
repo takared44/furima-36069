f96d2546e484e50aafceb8e478848068a770da67
takared44 <86856476+takared44@users.noreply.github.com>
2021-08-05 17:53:04 +0900

    エラー出たので戻しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,10 +7,10 @@ class ItemsController < ApplicationController
 
   # before_action :sold_out, only: [:edit]
 
-  # def index
-  #   # 記事一覧を新規投稿順に並べる
-  #   @items = Item.all.order(id: 'DESC')
-  # end
+  def index
+    # 記事一覧を新規投稿順に並べる
+    @items = Item.all.order(id: 'DESC')
+  end
 
   def new
     @item = Item.new
