ff4f0984942a8e41cb7c358614ff380d4d86b525
takared44 <86856476+takared44@users.noreply.github.com>
2021-08-09 14:18:43 +0900

    contoller 削除修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,10 +41,10 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def destroy
-  #   @item.destroy
-  #   redirect_to root_path
-  # end
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
 
   private
 
