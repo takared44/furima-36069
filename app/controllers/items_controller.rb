11e445588512eec0a1128d892cc802433ebf3d0e
takared44 <86856476+takared44@users.noreply.github.com>
2021-08-13 20:04:38 +0900

    ログアウトできる。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:indx,:show]
+  before_action :authenticate_user!, except: [:index,:show]
 
   before_action :find_item, only: [:show, :edit, :update, :destroy]
 
